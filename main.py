028bb445fc2feb6f0dfaa42df683b573de6ca503
Maxio <91082344+MaximCosta@users.noreply.github.com>
2022-08-15 17:45:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from git import Repo
 
 PATH_OF_GIT_REPO = r"./race-pi-maxio-1/.git"
 FILE_MAX_SIZE = 100000000
+PUSH_OVER = 10000
 
 
 def get_digit(start: int) -> Iterator[int]:
@@ -37,33 +38,37 @@ def process(path: str, start: int) -> None:
     getter_pi: Iterator[int] = get_digit(start)
     repo = Repo(PATH_OF_GIT_REPO)
     current_file: "str" = sorted(filter(lambda x: ".txt" in x, os.listdir(path)), key=lambda x: int(x.split(".")[1]))[-1]
-    to_push: int = int(input(f"> "))
+    to_push: int = int(input(f"nb commit (choice * {PUSH_OVER})> ")) * PUSH_OVER
     file_changed: bool = True
     check: int = 0
     commited: int = 0
-    with tqdm(total=to_push) as pbar:
-        while commited < to_push:
-            if file_changed:
-                check = check_file(path, current_file)
-                file_changed = False
-                if check == FILE_MAX_SIZE:
-                    repo.remote(name="origin").push()
-                    print(f"{current_file} is full")
-                    current_file = f"pi.{int(current_file.split('.')[1]) + 1}.txt"
-                    print(f"{current_file} is now the current file")
-                    file_changed = True
-                    continue
-            check = FILE_MAX_SIZE - check
-            while check != 0 and commited < to_push:
-                char: str = str(next(getter_pi))
-                open(f"./{path}/{current_file}", "a").write(char)
-                check -= 1
-                commited += 1
-                pbar.update(1)
-                repo.git.add(update=True)
-                repo.index.commit(f"decimal: {start + commited} ({char})")
-            file_changed = True
-        repo.remote(name="origin").push()
+    with tqdm(total=to_push // PUSH_OVER) as pbarPush:
+        with tqdm(total=min(PUSH_OVER, to_push - commited), leave=False) as pbarCommit:
+            while commited < to_push:
+                if file_changed:
+                    check = check_file(path, current_file)
+                    file_changed = False
+                    if check == FILE_MAX_SIZE:
+                        print(f"{current_file} is full")
+                        current_file = f"pi.{int(current_file.split('.')[1]) + 1}.txt"
+                        print(f"{current_file} is now the current file")
+                        file_changed = True
+                        continue
+                check = FILE_MAX_SIZE - check
+                while check != 0 and commited < to_push:
+                    char: str = str(next(getter_pi))
+                    open(f"./{path}/{current_file}", "a").write(char)
+                    check -= 1
+                    commited += 1
+                    pbarCommit.update(1)
+                    repo.git.add(update=True)
+                    repo.index.commit(f"decimal: {start + commited} ({char})")
+                    if commited % PUSH_OVER == 0:
+                        repo.remote(name="origin").push()
+                        pbarPush.update(1)
+                        pbarCommit.reset()
+                file_changed = True
+            repo.remote(name="origin").push()
 
 
 def main() -> None:
